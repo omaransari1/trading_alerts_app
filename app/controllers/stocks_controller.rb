2e2107ffd12abdc77cbbe2874eaab3c4daca8896
Omar Ansari <omaransari1@gmail.com>
2017-06-15 03:29:55 -0500

    Add 50 wk MA and 52 week high columns to stocks table

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,8 @@
 class StocksController < ApplicationController
   def index
     @yahoo_client = YahooFinance::Client.new
-    @data = @yahoo_client.quotes(['PCLN', 'AMZN','AAPL'], [:ask, :bid, :last_trade_date])
+    @data = @yahoo_client.quotes(['PCLN', 'AMZN','AAPL'], [:ask, :bid, :moving_average_50_day,
+      :high_52_weeks])
     render 'index.html.erb'
   end
 end
