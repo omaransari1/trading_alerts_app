df291c0a11e295fa9f79a8824e147b5292b050ec
Omar Ansari <omaransari1@gmail.com>
2017-06-14 20:14:48 -0500

    Test YahooFinance api in stocks controller/view

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,7 @@
 class StocksController < ApplicationController
   def index
+    @yahoo_client = YahooFinance::Client.new
+    @data = @yahoo_client.quotes(['PCLN', 'AMZN','AAPL'], [:ask, :bid, :last_trade_date])
     render 'index.html.erb'
   end
 end
