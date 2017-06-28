7766ce3c7440cc2f08787b06a6dcac9ffcf76a07
Omar Ansari <omaransari1@gmail.com>
2017-06-27 21:18:58 -0500

    Add another column to table

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,7 @@
 class StocksController < ApplicationController
   def index
     @yahoo_client = YahooFinance::Client.new
-    @data = @yahoo_client.quotes(['PCLN', 'AMZN','AAPL'], [:ask, :bid, :moving_average_50_day,
+    @data = @yahoo_client.quotes(['PCLN', 'AMZN','AAPL'], [:last_trade_price, :bid, :moving_average_50_day,
       :high_52_weeks])
     render 'index.html.erb'
   end
