a0f2bec895553f1c75ade04444075962c3b7476b
Omar Ansari <omaransari1@gmail.com>
2017-07-22 22:01:00 -0500

    Add approprite uniqueness validation to Stock model (scope: user_id)

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,6 @@
 class Stock < ApplicationRecord
   belongs_to :user
 
-  validates :symbol, presence: true
+  validates :symbol, presence: true, uniqueness: { scope: :user_id,
+    message: "should happen once per user" }
 end
