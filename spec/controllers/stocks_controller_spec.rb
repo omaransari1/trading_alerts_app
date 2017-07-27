9ced93dd110ccf31c28c6813b5c4ce1bf4c15131
Omar Ansari <omaransari1@gmail.com>
2017-07-27 04:25:45 -0500

    Dry up controller spec with user double created 'before'

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 RSpec.describe StocksController do
+  before(:each) do
+    user = double('user', :id => 1)
+    allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+    allow(controller).to receive(:current_user).and_return(user)
+  end
   describe "GET index" do
     # it "assigns @teams" do
     #   team = Team.create
@@ -9,20 +14,13 @@ RSpec.describe StocksController do
     # end
 
     it "renders the index template" do
-      user = double('user', :id => 1)
-      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-      allow(controller).to receive(:current_user).and_return(user)
       get :index
       expect(response).to render_template('watchlist.html.erb')
     end
 
     it "returns an HTTP status code 200" do
-      user = double('user', :id => 1)
-      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-      allow(controller).to receive(:current_user).and_return(user)
       get :index
       expect(response).to have_http_status(200)
-
     end
   end
 end
